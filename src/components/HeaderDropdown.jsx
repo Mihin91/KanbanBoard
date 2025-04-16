d6440d6f4b7472784f5d6d8b70efaa17929bd9ed
Mihin <mihintabeywickrama55@gmail.com>
2025-04-16 19:10:52 +0530

    implemented dark mode and light mode and add reducers for board slices

--- src/components/HeaderDropdown.jsx
+++ src/components/HeaderDropdown.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import boardIcon from '../assets/icon-board.svg'
+import darkIcon from "../assets/icon-dark-theme.svg";
+import lightIcon from "../assets/icon-light-theme.svg";
+import { Switch } from '@headlessui/react';
+import useDarkMode from '../Hooks/useDarkMode'
 
 function HeaderDropdown({setOpenDropdown}) {
+  const [colorTheme , setTheme] = useDarkMode()
+  const [darkSide , setDarkSide] = useState(
+      colorTheme === 'light' ? true : false
+  )
 
-   const boards = useSelector ((state) => state.boards)
+  const toggleDarkMode = (checked) => {
+    setTheme(colorTheme)
+    setDarkSide(checked)
+  }
 
-   console.log('boards=',boards)
+  const boards = useSelector ((state) => state.boards)
+
 
   return (
     // when clicked outside it returns to normal
@@ -47,11 +59,30 @@ function HeaderDropdown({setOpenDropdown}) {
               Create New Board
             </p>
           </div>
+
+          <div className=' mx-2 p-4 space-x-2 bg-slate-100 dark:bg-[#20212c] flex justify-center items-center rounded-lg'>
+            <img src={lightIcon}/>
+
+            {/*Switch*/}
+            
+            <Switch 
+              checked={darkSide}
+              onChange={toggleDarkMode}
+              className={` ${darkSide ? ' bg-[#635fc7]':' bg-gray-200'}
+              relative inline-flex h-6 w-11 items-center rounded-full
+              `}>
+
+              <span className={`${ darkSide ? 'translate-x-6': 'translate-x-1'}
+              inline-block h-4 w-4 transform rounded-full bg-white transition
+              `}/>
+            </Switch>
+
+            <img src={darkIcon}/>
+          </div>
+
         </div>
 
       </div>
-
-
     </div>
   )
 }
