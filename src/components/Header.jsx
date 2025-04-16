69f031ed1dd1ba45b5b1c33cc5bd4f11895a2139
Mihin <mihintabeywickrama55@gmail.com>
2025-04-16 18:28:09 +0530

    created header related components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
             {/*Right side*/}
 
             <div className=' flex space-x-4 items-center md:space-x-6'>
-                <button className= ' button'>
+                <button className= ' hidden md:block button'>
                     + Add New Task
                 </button>
                 <button className=' button py-1 px-3 md:hidden'>
@@ -47,7 +47,7 @@ function Header() {
             </div>
         </header>
 
-        {openDropdown && <HeaderDropdown/>}
+        {openDropdown && <HeaderDropdown setOpenDropdown={setOpenDropdown}/>}
     </div>
   )
 }
